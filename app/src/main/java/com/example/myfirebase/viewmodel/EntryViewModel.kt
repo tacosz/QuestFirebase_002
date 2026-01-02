1a8c654c475d4fca5cc3f957ff7be6ede0cd6821
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2026-01-02 14:17:16 +0700

    fungsi private untuk validasi input

--- app/src/main/java/com/example/myfirebase/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/EntryViewModel.kt
@@ -8,4 +8,11 @@ import androidx.compose.runtime.setValue
 class EntryViewModel(private val repositorySiswa: RepositorySiswa): ViewModel() {
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean
+    {
+        return with(uiState){
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
 }
