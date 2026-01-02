47f4c2f4ae1ff6cb9d219124f5d680286acf4c4b
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2026-01-02 14:59:35 +0700

    siswa

--- app/src/main/java/com/example/myfirebase/modeldata/Siswa.kt
+++ app/src/main/java/com/example/myfirebase/modeldata/Siswa.kt
@@ -14,10 +14,14 @@ data class DetailSiswa(
     val telpon: String = ""
 )
 
-fun DetailSiswa.toDataSiswa(): Siswa(id, nama, alamat, telpon)
+fun DetailSiswa.toDataSiswa(): Siswa = Siswa(id, nama, alamat, telpon)
 fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(id, nama, alamat, telpon)
 
 data class UIStateSiswa(
     val detailSiswa: DetailSiswa = DetailSiswa(),
     val isEntryValid: Boolean = false
+)
+fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
 )
