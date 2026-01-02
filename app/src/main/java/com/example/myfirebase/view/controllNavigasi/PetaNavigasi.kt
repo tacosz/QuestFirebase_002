b9fc11eabba0944d2ecb7397071555450517625c
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2026-01-02 14:32:55 +0700

    fungsi data siswa app

--- app/src/main/java/com/example/myfirebase/view/controllNavigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/myfirebase/view/controllNavigasi/PetaNavigasi.kt
@@ -1,2 +1,11 @@
 package com.example.myfirebase.view.controllNavigasi
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun DataSiswaApp(navController: NavHostController = rememberNavController(), modifier: Modifier){
+    HostNavigasi(navController = navController)
+}
