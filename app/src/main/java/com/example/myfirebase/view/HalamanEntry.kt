91645adaeb3c7a0ef0f4e8c6d225cee14e386f05
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2026-01-02 15:27:23 +0700

    entry siswa body pada fungsi entry siswa screen

--- app/src/main/java/com/example/myfirebase/view/HalamanEntry.kt
+++ app/src/main/java/com/example/myfirebase/view/HalamanEntry.kt
@@ -1,5 +1,9 @@
 package com.example.myfirebase.view
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
@@ -12,6 +16,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.myfirebase.view.route.DestinasiEntry
 import com.example.myfirebase.viewmodel.EntryViewModel
 import com.example.myfirebase.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -32,5 +37,20 @@ fun EntrySiswaScreen(
                 scrollBehavior = scrollBehavior
             )
         }
-    ) {  }
+    ) { innerPadding ->
+        EntrySiswaBody(
+            uiStateSiswa = viewModel.uiStateSiswa,
+            onSiswaValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.addSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
 }
