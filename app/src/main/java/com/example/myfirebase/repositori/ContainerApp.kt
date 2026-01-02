4ac10b1a1ffe9224bc3944b33912183ea142d05d
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2026-01-02 13:57:53 +0700

    variabel override untuk repository siswa unt default container app

--- app/src/main/java/com/example/myfirebase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/myfirebase/repositori/ContainerApp.kt
@@ -3,3 +3,9 @@ package com.example.myfirebase.repositori
 interface ContainerApp {
     val repositorySiswa: ReposirotySiswa
 }
+
+class DefaultContainerApp : ContainerApp {
+    override val repositorySiswa: ReposirotySiswa by lazy {
+        FirebaseRepositorySiswa()
+    }
+}
