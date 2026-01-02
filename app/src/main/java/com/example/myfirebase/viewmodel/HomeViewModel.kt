1a5c41fcf2dc9c2fcb4b2f44f0a1c7c6dbe03755
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2026-01-02 14:26:37 +0700

    class home view model berisi variabel status ui siswa

--- app/src/main/java/com/example/myfirebase/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/HomeViewModel.kt
@@ -1,7 +1,16 @@
 package com.example.myfirebase.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
 sealed interface StatusUiSiswa {
     data class Success(val siswa: List<Siswa> = listOf()) : StatusUiSiswa
     object Error : StatusUiSiswa
     object Loading : StatusUiSiswa
+}
+class HomeViewModel(private val repositorySiswa: RepositorySiswa): ViewModel() {
+    var statusUiSiswa: StatusUiSiswa by mutableStateOf(StatusUiSiswa.Loading)
+        private set
 }
