215177ee660d285bbe1a5144796aba9b378b603a
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2026-01-02 14:29:17 +0700

    fungsi creation extra pada aplikasi data siswa

--- app/src/main/java/com/example/myfirebase/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/PenyediaViewModel.kt
@@ -1,2 +1,10 @@
 package com.example.myfirebase.viewmodel
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import com.example.myfirebase.repositori.AplikasiDataSiswa
+
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
+        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
+        AplikasiDataSiswa
+        )
