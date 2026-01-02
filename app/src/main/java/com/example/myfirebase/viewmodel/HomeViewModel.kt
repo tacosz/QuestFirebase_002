f41d69940cc39b1a9a1c5f003b1db9f67c254c0c
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2026-01-02 14:24:21 +0700

    sealed interface untuk sttaus ui siswa berisi data class sukses, error dan loading

--- app/src/main/java/com/example/myfirebase/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/HomeViewModel.kt
@@ -1,2 +1,7 @@
 package com.example.myfirebase.viewmodel
 
+sealed interface StatusUiSiswa {
+    data class Success(val siswa: List<Siswa> = listOf()) : StatusUiSiswa
+    object Error : StatusUiSiswa
+    object Loading : StatusUiSiswa
+}
