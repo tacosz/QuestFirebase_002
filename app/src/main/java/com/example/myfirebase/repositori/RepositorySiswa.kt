950699fc1d5b5b0ab9ca781378fd90faa547f757
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2026-01-02 14:47:18 +0700

    fungsi get data siswa diambil dari list siswa

--- app/src/main/java/com/example/myfirebase/repositori/RepositorySiswa.kt
+++ app/src/main/java/com/example/myfirebase/repositori/RepositorySiswa.kt
@@ -1,6 +1,29 @@
 package com.example.myfirebase.repositori
 
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.protobuf.LazyStringArrayList.emptyList
+import kotlinx.coroutines.tasks.await
+
 interface RepositorySiswa {
     suspend fun getDataSiswa(): List<Siswa>
     suspend fun postDataSiswa(siswa: Siswa)
+}
+class FirebaseRepositorySiswa : RepositorySiswa {
+    private val db = FirebaseFirestore.getInstance()
+    private val collection = db.collection("siswa")
+
+    override suspend fun getDataSiswa(): List<Siswa> {
+        return try {
+            collection.get().await().documents.map { doc ->
+                Siswa(
+                    id = doc.getLong("id")?.toLong() ?: 0,
+                    nama = doc.getString("nama") ?: "",
+                    alamat = doc.getString("alamat") ?: "",
+                    telpon = doc.getString("telpon") ?: ""
+                )
+            }
+        } catch (e: Exception){
+            emptyList()
+        }
+    }
 }
