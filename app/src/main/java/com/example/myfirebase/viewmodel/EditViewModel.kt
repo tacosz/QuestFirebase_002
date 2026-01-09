05e839b87f87e3a4ffcdca1660ad02eb78e70b8d
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2026-01-09 19:30:11 +0700

    class edit view model terdapat variable private repositori siswa didalamnya

--- app/src/main/java/com/example/myfirebase/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/EditViewModel.kt
@@ -1,2 +1,9 @@
 package com.example.myfirebase.viewmodel
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.myfirebase.repositori.RepositorySiswa
+
+class EditViewModel(savedStateHandle: SavedStateHandle, private val repositorySiswa: RepositorySiswa): ViewModel(){
+
+}
