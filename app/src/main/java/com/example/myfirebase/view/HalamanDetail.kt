bf84a1f697877c65991b7fcc5e12f25ce4152511
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2026-01-09 20:09:54 +0700

    button untuk konfirmasi hapus

--- app/src/main/java/com/example/myfirebase/view/HalamanDetail.kt
+++ app/src/main/java/com/example/myfirebase/view/HalamanDetail.kt
@@ -12,7 +12,9 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -93,6 +95,13 @@ private fun BodyDetailDataSiswa(
                 siswa = statusUIDetail.satusiswa,
                 modifier = Modifier.fillMaxWidth())
             else -> {}
+        }
+        OutlinedButton(
+            onClick = {deleteConfirmationRequired = true},
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(stringResource(R.string.delete))
         }
     }
 }
