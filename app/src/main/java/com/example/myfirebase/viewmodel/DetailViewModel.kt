87515245566a88dedfd64482d5299738130b7838
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2026-01-09 16:51:18 +0700

    sealed interface status ui detail berisi data class sukses, object error dan loading

--- app/src/main/java/com/example/myfirebase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/DetailViewModel.kt
@@ -1,2 +1,12 @@
+@file:OptIn(InternalSerializationApi::class)
+
 package com.example.myfirebase.viewmodel
 
+import com.example.myfirebase.modeldata.Siswa
+import kotlinx.serialization.InternalSerializationApi
+
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: Siswa?) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
