f848541ed92c174cdf6ec5c09c2c562e8135e166
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2026-01-09 19:32:39 +0700

    variable ui state siswa dan private set untuk mengubah nilai dalam kelas yang sama

--- app/src/main/java/com/example/myfirebase/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/EditViewModel.kt
@@ -1,9 +1,14 @@
 package com.example.myfirebase.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import com.example.myfirebase.modeldata.UIStateSiswa
 import com.example.myfirebase.repositori.RepositorySiswa
 
 class EditViewModel(savedStateHandle: SavedStateHandle, private val repositorySiswa: RepositorySiswa): ViewModel(){
-
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
 }
