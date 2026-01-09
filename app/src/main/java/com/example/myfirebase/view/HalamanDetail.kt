6f04618780ffca9996e8f97ec7a45d4db70a22de
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2026-01-09 19:52:51 +0700

    topbar di dalam scaffold

--- app/src/main/java/com/example/myfirebase/view/HalamanDetail.kt
+++ app/src/main/java/com/example/myfirebase/view/HalamanDetail.kt
@@ -1,9 +1,12 @@
 package com.example.myfirebase.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.myfirebase.view.route.DestinasiDetail
 import com.example.myfirebase.viewmodel.DetailViewModel
 import com.example.myfirebase.viewmodel.PenyediaViewModel
 
@@ -14,4 +17,14 @@ fun DetailSiswaScreen(
     navigateBack: () -> Unit,
     modifier: Modifier = Modifier,
     viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
-)
+){
+    Scaffold(
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiDetail.titleRes),
+                canNavigateBack = true,
+                navigateUp = navigateBack
+            )
+        },
+    ) {  }
+}
