c13c43d66b06ce66207c131859688835609da0de
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2026-01-09 20:53:36 +0700

    initializer dalam variable factory detail dan edit view model

--- app/src/main/java/com/example/myfirebase/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.myfirebase.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -14,5 +15,7 @@ object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomeViewModel(aplikasiDataSiswa().container.repositorySiswa) }
         initializer { EntryViewModel(aplikasiDataSiswa().container.repositorySiswa) }
+        initializer { DetailViewModel(this.createSavedStateHandle(),aplikasiDataSiswa().container.repositorySiswa) }
+        initializer { EditViewModel(this.createSavedStateHandle(),aplikasiDataSiswa().container.repositorySiswa) }
     }
 }
