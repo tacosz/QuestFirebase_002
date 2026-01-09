9461e07a87f593f358545153bf03fae764ea0e42
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2026-01-09 20:59:26 +0700

    composable route destinasi detail

--- app/src/main/java/com/example/myfirebase/view/controllNavigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/myfirebase/view/controllNavigasi/PetaNavigasi.kt
@@ -1,14 +1,19 @@
 package com.example.myfirebase.view.controllNavigasi
 
+import android.R.attr.type
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.myfirebase.view.DetailSiswaScreen
 import com.example.myfirebase.view.EntrySiswaScreen
 import com.example.myfirebase.view.HomeScreen
 import com.example.myfirebase.view.route.DestinasiDetail
+import com.example.myfirebase.view.route.DestinasiEdit
 import com.example.myfirebase.view.route.DestinasiEntry
 import com.example.myfirebase.view.route.DestinasiHome
 
@@ -34,6 +39,14 @@ fun HostNavigasi(
         composable(DestinasiEntry.route){
             EntrySiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route)
             })
+        }
+        composable(DestinasiDetail.routeWithArgs, arguments = listOf(navArgument
+            (DestinasiDetail.itemIdArg){
+            type = NavType.StringType
+        })
+        ){
+            DetailSiswaScreen(navigateToEditItem = {navController.navigate("${DestinasiEdit.route}/$it")},
+                navigateBack = {navController.navigate(DestinasiHome.route)})
         }
     }
 }
