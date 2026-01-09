cbdc57b4f14b5ca02be824c3151b56fcaf849edd
tacosz <nabil.nasruddin.ft23@mail.umy.ac.id>
2026-01-09 20:19:22 +0700

    baris detail data nama

--- app/src/main/java/com/example/myfirebase/view/HalamanDetail.kt
+++ app/src/main/java/com/example/myfirebase/view/HalamanDetail.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -120,5 +122,25 @@ private fun BodyDetailDataSiswa(
 fun DetailDataSiswa(
     siswa: Siswa?, modifier: Modifier = Modifier
 ){
-
+    Card(
+        modifier = modifier, colors = CardDefaults.cardColors(
+            contentColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ){
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(id = R.dimen.padding_medium)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+        ) {
+            BarisDetailData(
+                labelResID = R.string.nama1,
+                itemDetail = siswa!!.nama,
+                modifier = Modifier.padding(
+                    horizontal = dimensionResource(id = R.dimen.padding_medium)
+                )
+            )
+        }
+    }
 }
